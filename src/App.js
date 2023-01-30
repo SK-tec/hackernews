4b9579191516e3645254250cdc653a0bc91d0da0
SK-tec <samathagamidi@gmail.com>
2023-01-30 17:23:27 +0100

    Hackernews project basic skeleton

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1 style={{color:'blue'}}>Welcome to <strong>Hacker News</strong>  Project</h1>
+    </div>
+  );
+}
+
+export default App;
