e69c34ae6ece01410636187336e5d7ce2e28aae2
Wiebke <wiebke.feldt@gmail.com>
2023-02-02 16:26:35 +0100

    added styling to output

--- src/PostsList.js
+++ src/PostsList.js
@@ -1,23 +1,32 @@
 import React from "react";
+import Card from "react-bootstrap/Card";
 
 function PostsList({ news }) {
   return (
-    <div>
+    <div className="container">
       {news.map((post) => (
-        <div key={post.objectID}>
-          <div>
-            {post.points} point{post.points === 1 && "s"}
-          </div>
-          <h1>{post.title}</h1>
-          <a href={post.url} target="_blank">
-            {post.url}
-          </a>
-          <div>{post.author}</div>
-          <div>{post.created_at}</div>
-          <div>
-            {post.num_comments} comment{post.num_comments === 1 && "s"}
-          </div>
-        </div>
+        <Card key={post.objectID} className="my-3">
+          <Card.Header className="d-flex justify-content-between">
+            <div>
+              {post.points} point{post.points !== 1 && "s"}
+            </div>
+            <div>{post.author}</div>
+            <div>
+              {post.num_comments} comment{post.num_comments !== 1 && "s"}
+            </div>
+            <div>{post.created_at}</div>
+          </Card.Header>
+          <Card.Body className="text-start">
+            <Card.Title>{post.title}</Card.Title>
+            <a
+              href={post.url}
+              target="_blank"
+              className="link-secondary text-decoration-none"
+            >
+              {post.url}
+            </a>
+          </Card.Body>
+        </Card>
       ))}
     </div>
   );
