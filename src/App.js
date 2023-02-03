88d8a57ac980bc3c9f6831f7b6129154b02e81ba
Wiebke <wiebke.feldt@gmail.com>
2023-02-03 12:43:44 +0100

    footer sticks to bottom, when no content is present

--- src/App.js
+++ src/App.js
@@ -4,23 +4,18 @@ import Header from "./components/Header";
 import Searchbar from "./components/Searchbar";
 import Footer from "./components/Footer";
 
-
 import { useState } from "react";
 function App() {
   const [news, setNews] = useState([]);
 
   return (
-    <div className="App">
-     <Header />
-      <h1 style={{ color: "blue" }}>
-        Welcome to <strong>Hacker News</strong>
-      </h1>
+    <div className="App ">
+      <Header />
       <div>
-       
         <Searchbar news={news} setNews={setNews} />
-        <PostsList news={news} />        
+        <PostsList news={news} />
       </div>
-      <Footer />
+      <Footer content={news.length} />
     </div>
   );
 }
