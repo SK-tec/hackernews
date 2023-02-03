0662240e43550f8accd6d35c3df4ffb9c015a8f6
SK-tec <samathagamidi@gmail.com>
2023-02-03 14:19:34 +0100

    spinner component created

---
+++ src/components/Spinner.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+const Spinner = () => {
+    return <>
+    <img
+        src="https://powerusers.microsoft.com/t5/image/serverpage/image-id/118082i204C32E01666789C/image-size/large/is-moderation-mode/true?v=v2&px=999"
+        alt="spinner"
+      />
+    </>
+}
+ 
+export default Spinner;
